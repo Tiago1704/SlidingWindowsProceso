ba7bfefca43a3e2c3e2cd2f8bc6d98d3264a2dc6
Tiago1704 <36915137+Tiago1704@users.noreply.github.com>
2019-12-19 20:34:09 -0300

    Add files via upload

---
+++ SlidingWindowsFinalizado/Main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 19 20:24:54 2019
+
+@author: tiago
+"""
+import SlidingWindows5
+        
+    #MuestraDeVectores(VDeSon1, VDeSon2)
+    #ComparacionDeVectores(VDeSon1, VDeSon2, Frec1, Frec2, ASon1, ASon2, Tamano1, Tamano2, TamEnSeg1, TamEnSeg2)
+    #MostrandoArrays(ASon1, ASon2)
+    #SliWin(ASon1, ASon2, TamaArray1, TamaArray2)
+    #MostrandoArraySliWindows(ASon1, ASon2, ASliWin, TamaArray1, TamaArray2)
+    
+    
---
+++ SlidingWindowsFinalizado/Readme.txt
@@ -0,0 +1,8 @@
+Los audios que se analizarán deben estar en la misma carpeta que el main.
+
+La función importante es SliWin, ella hace todo el procedimiento.
+Hay algunas extras agregadas, esas son:
+
+MuestraDeVectores: Muestra el arreglo de cada audio.
+ComparacionDeVectores: Compara cosas extras, como la frecuencia, o el tamaño.
+MostrandoArrays: Solamente Muestra los arrays.
